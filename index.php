90e9e9210cf16192f024aa9b26b42ad75a55705a
Quentin Bergougnoux <quentin.bergougnoux@gmail.com>
2016-05-18 16:02:58 +0200

    ?

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-GB">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=http://mrxedac.github.io/riku">
+        <script type="text/javascript">
+            window.location.href = "http://mrxedac.github.io/riku"
+        </script>
+        <title>Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you're not redirected, don't hesitate to follow this <a href='http://example.com'>link</a>.
+    </body>
+</html>
